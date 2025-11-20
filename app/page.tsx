ce7a2f5e0311d4d2fe6c81cf814a73655ca7f9f8
Ilya Hvostenko <ilyajavadev@gmail.com>
2025-11-20 09:47:54 +0200

    second commit navbar and events

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import {events} from "@/lib/constants";
+
+
+
 
 const Page = () => {
     return (
-       <section>
-           <h1 className='text-center'>The Hub for Every Dev <br/> Event You Can't Miss </h1>
-     <p className='text-center mt-5'> Hackathon, Meetups, and Conferences, All in One Place </p>
-       <ExploreBtn/>
+        <section>
+            <h1 className='text-center'>The Hub for Every Dev <br/> Event You Can't Miss </h1>
+            <p className='text-center mt-5'> Hackathon, Meetups, and Conferences, All in One Place </p>
+            <ExploreBtn/>
 
-           <div className='mt-20 space-y-7'>
-<h3>Feature Events</h3>
-               <ul className='events'>
-                   {[1,2,3,4,5].map((event) => (
-                       <li key={event}>Event {event}</li>
-                   ))}
-               </ul>
+            <div className='mt-20 space-y-7'>
+                <h3>Feature Events</h3>
+                <ul className='events'>
+                    {events.map((event) => (
 
-           </div>
+                        <li key={event.title}>
+                            <EventCard {...event}/>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
 
 
-       </section>
+        </section>
     )
 }
 export default Page
